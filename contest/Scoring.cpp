6128ae4d6a4a59cbb52a5a7da23f4f641b835d07
Rakesh Biswas <rbiswas01999@gmail.com>
2026-01-01 10:34:15 +0600

    :

---
+++ contest/Scoring.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;cin>>t;
+    while (t--)
+    {
+        
+        int a,b,x,y;cin>>x>>y;
+        cout<<(x+y)/2<<" ";
+        cout<<(y-x)/2<<endl;
+        
+    }
+
+    return 0;   
+}
