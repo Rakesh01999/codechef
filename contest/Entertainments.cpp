7026001e95f8fb29b7b72cfde829da247f3c4074
Rakesh Biswas <rbiswas01999@gmail.com>
2026-01-01 10:28:51 +0600

    entertainment

---
+++ contest/Entertainments.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    if((n*200 >= 1000))
+        cout<<1000<<endl;
+    else
+        cout<<(n*200)<<endl;
+    return 0;
+}
--- contest/New_Operation.cpp
+++ contest/New_Operation.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
+    int t;cin >> t;
+    while (t--) {
 
         int n;
         cin >> n;
