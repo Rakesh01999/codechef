cb7750e1806a4be0d40f7cba54cf1617e296a5dc
Rakesh Biswas <rbiswas01999@gmail.com>
2026-01-23 11:22:17 +0600

    Candy Types

--- contest/Candy_Types.cpp
+++ contest/Candy_Types.cpp
@@ -3,27 +3,28 @@ using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
+    int t;cin >> t;
+    while (t--){
+        int n;cin >> n;
         map<int, int> mp;
         vector<int> v;
-        for (int i = 0; i < n; i++)
-        {
-            int x;
-            cin >> x;
+        for (int i = 0; i < n; i++){
+            int x;cin >> x;
             mp[x]++;
         }
-        auto it = prev(mp.end());
-        // cout << it->first << " " << it->second << endl;
 
-        for (auto itr : mp)
-        {
+        int max = 0;
+        for(auto it1:mp){
+            if(it1.second > max){
+                max = it1.second;
+            }
+        }
+
+        // cout<<"max : "<<max<<endl;
+        
+        for (auto itr : mp){
             // cout<<itr.first<<" "<<itr.second<<endl;
-            if (itr.second == it->second)
+            if (itr.second == max)
             {
                 v.push_back(itr.first);
             }
