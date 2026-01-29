70fa65dae0888fc81694c4e8a56c886fe32002e6
Rakesh Biswas <rbiswas01999@gmail.com>
2026-01-29 09:49:50 +0600

    🚌 School Trip

--- School_Trip.cpp
+++ School_Trip.cpp
@@ -1,33 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
-    int n,x, k;cin>>n>>x>>k;
-    
-    if(x%k==0){
-        cout<<0<<endl;
+void solve() {
+    int n, x, k;cin >> n >> x >> k;
+
+    int ans = INT_MAX;
+
+    for (int i = 0; i <= n; i += k) {
+        ans = min(ans, abs(x - i));
     }
-    else{
-        if(k>x){
-            int ans1 = min(x, (n-k));
-            // cout<<"ans1 :"<<ans1<<endl;
-            int ans = min(abs(k-x), ans1);
-            cout<<ans<<endl;
-        }
-        else if(x>k){
-            int r = x%k;
-            int res = k*r - x;
-            int ans = min(abs(k-x), min((n-k), min(k, k-res)));
-            cout<<ans<<endl;
-        }
-    }
+
+    cout << ans << endl;
 }
 
-int main(){
-    
-    int t;cin>>t;
-    while(t--){
+int main() {
+    int t;cin >> t;
+    while (t--) {
         solve();
     }
+
+    
     return 0;
 }
